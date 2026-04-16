927da1baa588cef202569b32b7236a4a6e85d5c8
Thomas Shadforth <55760891+ThomasShadforth@users.noreply.github.com>
2026-04-16 11:11:13 +0100

    Create static_collapsible.js

---
+++ src/javascript/static_collapsible.js
@@ -0,0 +1,13 @@
+var coll = document.getElementsByClassName("staticCollapsible")
+var i;
+
+for(i = 0; i < coll.length; i++){
+  coll[i].addEventListener("click", function(){
+    this.classList.toggle("active");
+    if(content.style.display === "block"){
+      content.style.display = "none";
+    } else{
+      content.style.display = "block";
+    }
+  });
+}
